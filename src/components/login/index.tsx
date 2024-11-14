f89518f1b6e3c547bd8e0a715fb947be572bcc82
Pragya Khanna <khannp3@mcmaster.ca>
2024-11-14 03:17:25 -0500

    Initial login page done

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import { LoginBackground, LoginPanel } from './style'
+import {
+  LoginBackground,
+  LoginPanel,
+  Title,
+  Input,
+  Button,
+  Label,
+  FooterText,
+  DividerLine,
+} from './style'
 
 export default function LoginPane({ isLoggedIn, setLoggedIn }: any) {
   return (
     <LoginBackground>
       <LoginPanel>
-        Make this the login pane
-        <button onClick={() => setLoggedIn(!isLoggedIn)}>Log in</button>
+        <Title>Sign In</Title>
+        <Label>Email</Label>
+        <Input type="email" placeholder="Email" />
+        <Label>Password</Label>
+        <Input type="password" placeholder="Password" />
+        <Button onClick={() => setLoggedIn(!isLoggedIn)}>Sign in</Button>
+        <DividerLine />
+        <FooterText>
+          Need an Account? <a href="#">Sign up</a>
+        </FooterText>
       </LoginPanel>
     </LoginBackground>
   )
--- src/components/login/style.ts
+++ src/components/login/style.ts
@@ -6,14 +6,75 @@ export const LoginBackground = styled.div(() => {
   return {
     width: '100vw',
     height: '100vh',
-    backgroundColor: '#2e2c2f',
+    backgroundColor: '#29282A',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 })
 
 export const LoginPanel = styled.div(() => {
   return {
-    width: '20%',
+    width: '40%',
     height: '50%',
-    backgroundColor: '#29282A',
+    padding: '20px',
+    backgroundColor: '#E9E6E6',
+    alignItems: 'center',
+    textAlign: 'center',
+    borderRadius: '20px',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '10px',
   }
 })
+
+export const Title = styled.h2(() => ({
+  fontSize: '80px',
+  color: '#2E2C2F',
+  marginBottom: '15px',
+  marginTop: '40px',
+}))
+
+export const Label = styled.label(() => ({
+  width: '70%',
+  textAlign: 'left',
+  fontSize: '18px',
+  color: '#2E2C2F',
+  fontWeight: 'bold',
+}))
+
+export const Input = styled.input(() => ({
+  width: '70%',
+  height: '7%',
+  padding: '10px',
+  borderRadius: '10px',
+  border: '1px solid #E9E6E6',
+  fontSize: '16px',
+  marginBottom: '3px',
+}))
+
+export const Button = styled.button(() => ({
+  width: '30%',
+  padding: '10px',
+  borderRadius: '20px',
+  backgroundColor: '#44C7AF',
+  border: '1px solid #E9E6E6',
+  fontSize: '20px',
+  fontWeight: 'bold',
+  marginTop: '20px',
+}))
+
+export const DividerLine = styled.div(() => ({
+  width: '70%',
+  height: '2px',
+  backgroundColor: '#2E2C2F',
+  margin: '15px',
+}))
+
+export const FooterText = styled.p(() => ({
+  fontSize: '25px',
+  color: '#2E2C2F',
+  a: {
+    color: '#66C3FF',
+  },
+}))
