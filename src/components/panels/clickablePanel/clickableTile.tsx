4a792513ac33176b6da5c1269039a3c3aaa55076
Pragya Khanna <khannp3@mcmaster.ca>
2025-03-23 19:25:12 -0400

    Fixed resizing issue on tips page

--- src/components/panels/clickablePanel/clickableTile.tsx
+++ src/components/panels/clickablePanel/clickableTile.tsx
@@ -6,12 +6,8 @@ const TileWrapper = styled.div({
   transition: 'transform 0.3s ease, box-shadow 0.3s ease',
   // minWidth: '200px',
   borderRadius: '10px', // Rounded corners
-<<<<<<< HEAD
   overflow: 'hidden',
 
-=======
-  // overflow: 'hidden',
->>>>>>> 71abb28f3e09eb71a666835b24c6639e50a66278
   '&:hover': {
     transform: 'scale(1.05)',
     boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)',
