afd3c50baf1eaa24ae5a40db05236af94af1ba86
Cayden <90425128+ferguc18@users.noreply.github.com>
2025-03-23 15:09:04 -0400

    fixed styling format and graphing issue

--- src/app/tips/style.ts
+++ src/app/tips/style.ts
@@ -3,22 +3,24 @@
 import styled from '@emotion/styled'
 import { device } from '@/app/theme'
 
-export const TipsControlPanel = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 20px;
-  padding: 20px;
+export const TipsControlPanel = styled.div(() => {
+  return {
+    width: `100%`,
+    display: `grid`,
+    gridTemplateColumns: `1fr`,
+    gap: '20px',
+    padding: '20px',
 
-  ${device.sm} {
-    grid-template-columns: repeat(2, 1fr);
-  }
+    [device.sm]: {
+      gridTemplateColumns: `repeat(2, 1fr)`,
+    },
 
-  ${device.md} {
-    grid-template-columns: repeat(3, 1fr);
-  }
+    [device.md]: {
+      gridTemplateColumns: `repeat(3, 1fr)`,
+    },
 
-  ${device.lg} {
-    grid-template-columns: repeat(4, 1fr);
+    [device.lg]: {
+      gridTemplateColumns: `repeat(4, 1fr)`,
+    },
   }
-`
+})
